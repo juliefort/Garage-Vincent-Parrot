1a408d795be6e938ab0e18e52d4515b2bb5d37d3
juliefort <juliefort01@gmail.com>
2024-01-23 19:14:55 +0100

    added services section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Schedule;
+
 use App\Repository\ScheduleRepository;
+use App\Repository\ServicesRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,11 +11,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ScheduleRepository $scheduleRepo) : Response
+    public function index(
+        ScheduleRepository $scheduleRepo, ServicesRepository $servicesRepo
+        ) : Response
     {
 	    return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
-            'schedule' => $scheduleRepo->findAll() 
+            'schedule' => $scheduleRepo->findAll(),
+            'services' => $servicesRepo->findBy([],[])
         ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ScheduleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,7 @@ class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, ScheduleRepository $scheduleRepo): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
@@ -23,7 +24,11 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'schedule' => $scheduleRepo->findAll() 
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
