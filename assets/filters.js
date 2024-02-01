54e3350f0216717d5296bd0731a9b9ab555a1ac7
juliefort <juliefort01@gmail.com>
2024-02-01 19:58:39 +0100

    changed filters for cars

--- assets/filters.js
+++ assets/filters.js
@@ -1,23 +1,51 @@
 // Récupération de mes éléments du DOM 
 const toggleReset = document.querySelector('#toggleReset');
-const filterValue1 = document.querySelector('#filterValue1');
-const filterValue2 = document.querySelector('#filterValue2');
-const filterValue3 = document.querySelector('#filterValue3');
-const slider1 = document.querySelector('.slider1');
-const slider2 = document.querySelector('.slider2');
-const slider3 = document.querySelector('.slider3');
+const slider1 = document.querySelector('#slider1');
+const slider2 = document.querySelector('#slider2');
+const slider3 = document.querySelector('#slider3');
 const carPrice= document.querySelector('#carPrice');
 const carYear = document.querySelector('#carYear');
 const carKilometers = document.querySelector('#carKilometers');
-const carManufacturer = document.querySelector('#manufacturer');
-const carFuel = document.querySelector('#carFuel');
 
 // Réinitialisation des filtres
 toggleReset.addEventListener('click', () => {
     slider1.value = 0;
     slider2.value = 0;
     slider3.value = 0;
-})
+});
 
+const filteredCars = () => {
 
+    const getPrice = parseInt(slider1.value, 10);
+    const getKilometers = parseInt(slider2.value, 10);
+    const getYear = parseInt(slider3.value, 10);
 
+    let filter1 = document.querySelector('.getFilter1').innerHTML = `${getPrice} `;
+    let filter2 = document.querySelector('.getFilter2').innerHTML = `${getKilometers} km`;
+    let filter3 = document.querySelector('.getFilter3').innerHTML = getYear;  
+
+    const filters = document.querySelectorAll('div.filtered');
+
+    filters.forEach(filter => {
+        const price = parseInt(filter.getAttribute('data-price'), 10);
+        const kilometers = parseInt(filter.getAttribute('data-kilometers'), 10);
+        const year = parseInt(filter.getAttribute('data-year'), 10);
+
+        if(price <= getPrice &&  kilometers <= getKilometers &&  year <= getYear) 
+        {
+            filter.style.display = '';     
+        } else {
+            filter.style.display = 'none';
+        }
+
+       console.log(filters);
+});
+
+}
+
+slider1.addEventListener('input', filteredCars);
+slider2.addEventListener('input', filteredCars);
+slider3.addEventListener('input', filteredCars);
+    
+
+   
