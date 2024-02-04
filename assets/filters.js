547ed18914cb18ddb7bb9cf99d381dc88778122d
juliefort <juliefort01@gmail.com>
2024-02-04 21:55:04 +0100

    modified filters.js and home layout

--- assets/filters.js
+++ assets/filters.js
@@ -1,18 +1,11 @@
 // Récupération de mes éléments du DOM 
-const toggleReset = document.querySelector('#toggleReset');
 const slider1 = document.querySelector('#slider1');
 const slider2 = document.querySelector('#slider2');
 const slider3 = document.querySelector('#slider3');
 const carPrice= document.querySelector('#carPrice');
 const carYear = document.querySelector('#carYear');
 const carKilometers = document.querySelector('#carKilometers');
-
-// Réinitialisation des filtres
-toggleReset.addEventListener('click', () => {
-    slider1.value = 0;
-    slider2.value = 0;
-    slider3.value = 0;
-});
+const filters = document.querySelectorAll('div.filtered');
 
 const filteredCars = () => {
 
@@ -24,28 +17,41 @@ const filteredCars = () => {
     let filter2 = document.querySelector('.getFilter2').innerHTML = `${getKilometers} km`;
     let filter3 = document.querySelector('.getFilter3').innerHTML = getYear;  
 
-    const filters = document.querySelectorAll('div.filtered');
-
     filters.forEach(filter => {
         const price = parseInt(filter.getAttribute('data-price'), 10);
         const kilometers = parseInt(filter.getAttribute('data-kilometers'), 10);
         const year = parseInt(filter.getAttribute('data-year'), 10);
 
-        if(price <= getPrice &&  kilometers <= getKilometers &&  year <= getYear) 
+        if(price <= getPrice && kilometers <= getKilometers && year <= getYear) 
         {
             filter.style.display = '';     
         } else {
             filter.style.display = 'none';
         }
-
-       console.log(filters);
 });
+
 
 }
 
 slider1.addEventListener('input', filteredCars);
 slider2.addEventListener('input', filteredCars);
 slider3.addEventListener('input', filteredCars);
+   
+const toggleReset = document.querySelector('#toggleReset');
+
+// Réinitialisation des filtres 
+toggleReset.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    slider1.value = '25000';
+    slider2.value = '100000';
+    slider3.value = '2024';
+
     
 
+    filters.forEach(filter => {
+        filter.style.display = '';
+        filter.value = ''
+    })
+});
    
